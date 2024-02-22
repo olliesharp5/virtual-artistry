b2e2ec53be50f99603c450e354ef213433f2989c
Oliver Sharp <olliesharp5@gmail.com>
2024-02-22 18:21:34 +0000

    Amended ordering and meta in models

--- artwork/models.py
+++ artwork/models.py
@@ -20,6 +20,12 @@ class Art(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"The title of artwork post is {self.title}"
+
 
 class Review(models.Model):
     art = models.ForeignKey(
@@ -33,7 +39,7 @@ class Review(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['created_on']
+        ordering = ["created_on"]
 
     def __str__(self):
         return f"Review {self.body} by {self.author}"
