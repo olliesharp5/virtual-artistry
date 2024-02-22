476af779ea4e656f336c2b6b4bbb5f957e39abf5
Oliver Sharp <olliesharp5@gmail.com>
2024-02-22 17:19:40 +0000

    Added model for helprequest

--- help/models.py
+++ help/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class HelpRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Help request from {self.name}"
