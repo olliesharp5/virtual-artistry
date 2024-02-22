11983b4b39b6e7c07771a26ef23591a0c99f9cb8
Oliver Sharp <olliesharp5@gmail.com>
2024-02-22 17:04:27 +0000

    Migrated and added admin access for artists

--- artists/admin.py
+++ artists/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ArtistProfile
 
 # Register your models here.
+admin.site.register(ArtistProfile)
--- artists/models.py
+++ artists/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ArtistProfile(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.TextField()
+  # profile_image = CloudinaryField('image', default='placeholder')
+    about = models.TextField()
+
+    def __str__(self):
+        return self.name
