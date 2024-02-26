98b6868344db50c7f71eb5e08bfde98d883ebcc2
Oliver Sharp <olliesharp5@gmail.com>
2024-02-26 13:40:25 +0000

    Added slug field to artist profile model

--- artists/models.py
+++ artists/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class ArtistProfile(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
     location = models.TextField()
   # profile_image = CloudinaryField('image', default='placeholder')
     about = models.TextField()
