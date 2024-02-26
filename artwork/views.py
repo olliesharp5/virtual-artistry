e19b955c0094e07b276f2bc8f665f8f265c64807
Oliver Sharp <olliesharp5@gmail.com>
2024-02-26 14:56:51 +0000

    Added review form into modal

--- artwork/views.py
+++ artwork/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Art
+from .forms import ReviewForm
 
 # Create your views here.
 class ArtList(generic.ListView):
@@ -11,4 +12,10 @@ class ArtList(generic.ListView):
 
 def art_details(request, art_slug):
     art = get_object_or_404(Art, slug=art_slug)
-    return render(request, 'art_detail.html', {'art': art})
+    form = ReviewForm(request.POST or None)
+    if form.is_valid():
+        review = form.save(commit=False)
+        review.art = art
+        review.author = request.user  # Set the author to the current user
+        review.save()
+    return render(request, 'art_detail.html', {'art': art, 'review_form': form})
