d8abd5962ea59c66419e6415060a46f7c79b6b17
Oliver Sharp <olliesharp5@gmail.com>
2024-02-26 12:58:50 +0000

    Wired up art_detail to display artwork pages

--- artwork/urls.py
+++ artwork/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ArtList.as_view(), name='home'),
+    path('art/<slug:art_slug>/', views.art_details, name='art_details'),
 ]
--- artwork/views.py
+++ artwork/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Art
 
@@ -7,3 +7,8 @@ class ArtList(generic.ListView):
     queryset = Art.objects.all()
     template_name = "index.html"
     paginate_by = 6
+
+
+def art_details(request, art_slug):
+    art = get_object_or_404(Art, slug=art_slug)
+    return render(request, 'art_detail.html', {'art': art})
