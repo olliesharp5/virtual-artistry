0e89149b66b6119b70b679c283dd36e58c5922af
Oliver Sharp <olliesharp5@gmail.com>
2024-02-26 12:32:44 +0000

    Added artist_profile html and linked to tiles

--- artists/urls.py
+++ artists/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
    path('', views.ArtistList.as_view(), name='artists'),
+   path('artist/<int:artist_id>/', views.artist_profile, name='artist_profile'),
 ]
--- artists/views.py
+++ artists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import ArtistProfile
 
@@ -7,3 +7,8 @@ class ArtistList(generic.ListView):
     queryset = ArtistProfile.objects.all()
     template_name = "artists.html"
     paginate_by = 6
+
+
+def artist_profile(request, artist_id):
+    artist = get_object_or_404(ArtistProfile, pk=artist_id)
+    return render(request, 'artist_profile.html', {'artist': artist})
