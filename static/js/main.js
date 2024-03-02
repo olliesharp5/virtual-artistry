e438262360f6d6ffbe23f9ef6121c134816e0779
Oliver Sharp <olliesharp5@gmail.com>
2024-03-02 19:06:35 +0000

    Added general styles and color pallette to site

--- static/js/main.js
+++ static/js/main.js
@@ -15,3 +15,20 @@ $(document).ready(function(){
 $(document).ready(function(){
     $(".loading-item4").hide().fadeIn(2500);
 });
+
+$(document).ready(function(){
+    $(".nav-link").hover(
+        function(){
+            $(this).css({
+                "background-color": "#5e3c58",
+                "color": "#bfb5b2"
+            });
+        }, 
+        function(){
+            $(this).css({
+                "background-color": "transparent",
+                "color": "black"
+            });
+        }
+    );
+});
