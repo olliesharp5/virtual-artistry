e9d71d977a0410ffe4a10a0433ecdb7373d32aac
Oliver Sharp <olliesharp5@gmail.com>
2024-03-02 13:16:42 +0000

    Added funtionality to count likes and remove likes

--- artwork/views.py
+++ artwork/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib.auth.decorators import login_required
 from django.views import generic
 from django.contrib import messages
 from django.http import HttpResponseRedirect
@@ -26,6 +27,7 @@ class ArtList(generic.ListView):
 
 def art_details(request, art_slug):
     art = get_object_or_404(Art, slug=art_slug)
+    user_has_liked = Like.objects.filter(user=request.user, art=art).exists() if request.user.is_authenticated else False
     artist_profile = art.artist
     form = ReviewForm(request.POST or None)
     if form.is_valid():
@@ -34,18 +36,16 @@ def art_details(request, art_slug):
         review.author = request.user  # Set the author to the current user
         review.save()
     reviews = Review.objects.filter(art=art, approved=True)  # Fetch approved reviews
-    return render(request, 'art_detail.html', {'art': art, 'review_form': form, 'reviews': reviews, 'artist_profile': artist_profile,})
+    return render(request, 'art_detail.html', {'art': art, 'user_has_liked': user_has_liked, 'review_form': form, 'reviews': reviews, 'artist_profile': artist_profile,})
 
-
+@login_required
 def like_artwork(request, art_slug):
     artwork = get_object_or_404(Art, slug=art_slug)
-    Like.objects.get_or_create(user=request.user, art=artwork)
-    return redirect('art_details', art_slug=artwork.slug)
-
-
-def unlike_artwork(request, art_slug):
-    artwork = get_object_or_404(Art, slug=art_slug)
-    Like.objects.filter(user=request.user, art=artwork).delete()
+    like = Like.objects.filter(user=request.user, art=artwork)
+    if like.exists():
+        like.first().delete()
+    else:
+        Like.objects.create(user=request.user, art=artwork)
     return redirect('art_details', art_slug=artwork.slug)
 
 
