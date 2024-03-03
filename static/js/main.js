6ef9843eaa9e8ad9ab8916e74bb61dfc6fd28542
Oliver Sharp <olliesharp5@gmail.com>
2024-03-03 13:23:39 +0000

    Added cascading effect to cards using css and js

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,17 @@ $(document).ready(function(){
     $(".loading-item1, .loading-item2, .loading-item3, .loading-item4").css('opacity', '1');
 });
 
+// Gives each card a transition delay that increases by 0.2 seconds, creating a cascading effect.
+$(document).ready(function(){
+    $(".loading-cards").each(function(i) {
+        $(this).css({
+            'opacity': '1',
+            'transition': 'opacity 2.5s ease-in-out',
+            'transition-delay': (i * 0.2) + 's' // delay increases by 0.2s for each card
+        });
+    });
+});
+
 // JQuery Code 
 
 $(document).ready(function(){
