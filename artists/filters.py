135c2058548d9ac9c34d4189eebf0673ed3deea7
Oliver Sharp <olliesharp5@gmail.com>
2024-03-05 17:44:05 +0000

    Amended filter to display letters on artist page

--- artists/filters.py
+++ artists/filters.py
@@ -2,8 +2,21 @@ import django_filters
 from .models import ArtistProfile
 
 class ArtistProfileFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(field_name='name', lookup_expr='istartswith')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Generate a list of tuples for all uppercase letters from A to Z that have existing artists
+        self.filters['first_letter'].extra.update(
+            {
+                'choices': [(chr(i), chr(i)) for i in range(65, 91) if self.queryset.filter(name__istartswith=chr(i)).exists()],
+                 'empty_label': None,
+            }
+        )
+
+    first_letter = django_filters.ChoiceFilter(method='filter_by_first_letter')
 
     class Meta:
         model = ArtistProfile
-        fields = ['name']
+        fields = ['first_letter']
+
+    def filter_by_first_letter(self, queryset, name, value):
+        return queryset.filter(name__istartswith=value)
