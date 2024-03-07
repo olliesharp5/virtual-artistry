e849060fa5e961eb79a0eb8c295ad908532feef2
Oliver Sharp <olliesharp5@gmail.com>
2024-03-07 18:51:51 +0000

    Amended templates for artists to reflect new model layout

--- artists/urls.py
+++ artists/urls.py
@@ -4,5 +4,5 @@ from .views import ArtistList
 
 urlpatterns = [
     path('', ArtistList.as_view(), name='artists'),
-   path('artist/<str:username>/', views.artist_profile, name='artist_profile'),
+    path('artist/<str:username>/', views.artist_profile, name='artist_profile')
 ]
--- artists/views.py
+++ artists/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.views import generic
 from django_filters.views import FilterView
 from .models import UserProfile
@@ -15,7 +16,7 @@ class ArtistList(FilterView):
         return UserProfile.objects.filter(role='AR')
 
 
-def artist_profile(request, artist_slug):
+def artist_profile(request, username):
     user = get_object_or_404(User, username=username)
     artistprofile = UserProfile.objects.get(user=user)
     artworks = Art.objects.filter(artist=artistprofile)
