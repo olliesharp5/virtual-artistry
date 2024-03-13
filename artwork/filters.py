87bfeecf936de77b09cd46a23cfe0eff5e6a68f0
Oliver Sharp <olliesharp5@gmail.com>
2024-03-13 12:17:39 +0000

    Amended artist filter to show display_name instead of username

--- artwork/filters.py
+++ artwork/filters.py
@@ -1,9 +1,21 @@
 import django_filters
 from .models import Art
+from artists.models import UserProfile
+
+class DisplayNameChoiceFilter(django_filters.ChoiceFilter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.extra['choices'] = [(userprofile.display_name, userprofile.display_name) for userprofile in UserProfile.objects.all()]
+
+    def filter(self, qs, value):
+        if value:
+            return qs.filter(artist__display_name=value)
+        return qs
 
 class ArtFilter(django_filters.FilterSet):
     price_min = django_filters.NumberFilter(field_name="price", lookup_expr='gte')
     price_max = django_filters.NumberFilter(field_name="price", lookup_expr='lte')
+    artist = DisplayNameChoiceFilter()
 
     class Meta:
         model = Art
@@ -11,9 +23,6 @@ class ArtFilter(django_filters.FilterSet):
 
     def __init__(self, *args, **kwargs):
         super(ArtFilter, self).__init__(*args, **kwargs)
-        self.filters['artist'].extra.update(
-            {'empty_label': 'Any'}
-        )
         self.filters['condition'].extra.update(
             {'empty_label': 'Any'}
         )
