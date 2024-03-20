05c64afeb7055d76767edd7d92e8e9293961a4f8
Oliver Sharp <olliesharp5@gmail.com>
2024-03-20 16:57:29 +0000

    Added message to create art form and hid admin role on register

--- accounts/forms.py
+++ accounts/forms.py
@@ -18,6 +18,10 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['role', 'display_name', 'location', 'profile_image', 'about']
+    
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        self.fields['role'].choices = [(tag[0], tag[1]) for tag in UserProfile.USER_ROLES if tag[0] in ['RU', 'AR']]
 
 class UpdateProfileForm(UserProfileForm):
     class Meta(UserProfileForm.Meta):
