d3815905cbf536cf5167fb1f2c0aaac20cdec944
Oliver Sharp <olliesharp5@gmail.com>
2024-03-21 12:51:17 +0000

    Debug false

--- virtual_artistry/settings.py
+++ virtual_artistry/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-^q8^@^9a)$ysw6$q_($k*s29d74eb_b=d0#@q^8yuc3u5-!la)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-olliesharp5-virtualarti-98g3ie3ojvo.ws-eu108.gitpod.io', '8000-olliesharp5-virtualarti-98g3ie3ojvo.ws-eu110.gitpod.io',
                 '.herokuapp.com']
